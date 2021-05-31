1ea1bf64c678326664d7eab4b5d460636f5d2d52
Sammy <s.challinor3@gmail.com>
2021-05-31 20:22:49 +0100

    fixed mobile click hover issue again

--- readfrench/Resources/script.js
+++ readfrench/Resources/script.js
@@ -90,11 +90,7 @@ let frenchDictionary = [
 
 //instruct user to click not hover if on mobile
 if (window.innerWidth < 844) {
-    alert("it is less")
     document.getElementById('hoverorclick').innerHTML = "Click on any word to get started"
-}
-else {
-    alert(window.innerWidth)
 }
 
 //whenever a defined <span> is hovered over it should invoke translateWord() and pass it the word/phrase being hovered over
